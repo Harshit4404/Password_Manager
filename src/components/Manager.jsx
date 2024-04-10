30b2ad2cf64e0f82481b7914b19fad461a52ba36
Harshit <meenushukla987654321@gmail.com>
2024-04-10 23:02:47 +0530

    .

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -4,12 +4,32 @@ const Manager = () => {
     return (
     <> 
         <div>
-            <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+            <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#22c55e_100%)]"></div>
         </div>
         <div className="max-w-4xl mycontainer">
-            <h1>PaSsLAST</h1>
+            <h1 className='text-4xl font-bold text-zinc-800 text-center'><span >&lt;</span>
+                <span className='text-green-500'>
+                    P
+                </span>
+                a
+                <span className='text-green-500'>
+                    S
+                </span>
+                s
+                <span className='text-green-500'>
+                    L
+                </span>
+                a
+                <span className='text-green-500'>
+                    S
+                </span>
+                t
+                <span className='text-green-500'>/</span>
+                <span>&gt;</span></h1>
+            <p className='text-green-900 text-lg text-center'>Your Own Passsord Manager
+            </p>
             <div className='text-white flex flex-col p-4'>   
-                <input className="rounded-full" type="text" name='' id='' />
+                <input className="rounded-full border border-fuchsia" type="text" name='' id='' />
                 <div className="flex">
                     <input type="text" name="" id="" />
                     <input type="text" name="" id="" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-violet-950 text-white'>
+    <nav className='bg-zinc-800 text-white'>
         <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
 
             <div className="logo font-bold text-2xl">
                 <span >&lt;</span>
-                <span className='text-fuchsia-400'>
+                <span className='text-green-400'>
                     P
                 </span>
                 a
-                <span className='text-fuchsia-400'>
+                <span className='text-green-400'>
                     S
                 </span>
                 s
-                <span className='text-fuchsia-400'>
+                <span className='text-green-400'>
                     L
                 </span>
                 a
-                <span className='text-fuchsia-400'>
+                <span className='text-green-400'>
                     S
                 </span>
                 t
-                <span className='text-fuchsia-400'>/</span>
+                <span className='text-green-400'>/</span>
                 <span>&gt;</span>
                 
             </div>
