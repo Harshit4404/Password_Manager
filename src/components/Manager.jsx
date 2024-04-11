dd0566db4cf8b2e3f07f90373665ffbb95fad11e
Harshit <meenushukla987654321@gmail.com>
2024-04-11 11:27:04 +0530

    ui

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -6,8 +6,8 @@ const Manager = () => {
         <div>
             <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#22c55e_100%)]"></div>
         </div>
-        <div className="max-w-4xl mycontainer">
-            <h1 className='text-4xl font-bold text-zinc-800 text-center'><span >&lt;</span>
+        <div className="mycontainer">
+            <h1 className='text-4xl font-bold text-zinc-700 text-center'><span >&lt;</span>
                 <span className='text-green-500'>
                     P
                 </span>
@@ -28,13 +28,19 @@ const Manager = () => {
                 <span>&gt;</span></h1>
             <p className='text-green-900 text-lg text-center'>Your Own Passsord Manager
             </p>
-            <div className='text-white flex flex-col p-4'>   
-                <input className="rounded-full border border-fuchsia" type="text" name='' id='' />
-                <div className="flex">
-                    <input type="text" name="" id="" />
-                    <input type="text" name="" id="" />
+            <div className='text-black flex flex-col p-4 gap-8 items-center'>   
+                <input className="rounded-full border border-green-700 w-full py-1 px-4" type="text" name='' id='' />
+                <div className="flex w-full gap-4">
+                    <input className="rounded-full border border-green-700 w-full py-1 px-4" type="text" name='' id='' />
+                    <input className="rounded-full border border-green-700 w-full py-1 px-4" type="text" name='' id='' />
                 </div>
+                
+                <button className='flex justify-center items-center bg-green-600 hover:bg-green-400 w-fit gap-3 rounded-full py-1 px-5 border-2 hover:border border-green-800'><lord-icon className="text-white" 
+                    src="https://cdn.lordicon.com/jgnvfzqg.json"
+                    trigger="hover">
+                </lord-icon>Add Password</button>
             </div>
+
         </div>
         
     </>
